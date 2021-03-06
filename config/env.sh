50c1a333b044cdf47972c7afc51ac5fb81cea935
AndresSanG <asantiagowsc@gmail.com>
2021-03-06 11:17:21 -0600

    archivos inecearios

--- config/env.sh
+++
@@ -1,4 +0,0 @@
-export NODE_ENV='production'
-export PORT=3000
-export SECRET='secret'
-export MONGO_URI='mongodb+srv:/AndresSG:2406SanCrav@cluster0.yajp7.mongodb.net/users_database?retryWrites=true&w=majority`
