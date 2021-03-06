d8e0f90abef7022c349cc7988805e01971e62952
AndresSanG <asantiagowsc@gmail.com>
2021-03-06 11:03:43 -0600

    primer commit

---
+++ config/env.sh
@@ -0,0 +1,4 @@
+export NODE_ENV='production'
+export PORT=3000
+export SECRET='secret'
+export MONGO_URI='mongodb+srv:/AndresSG:2406SanCrav@cluster0.yajp7.mongodb.net/users_database?retryWrites=true&w=majority`
